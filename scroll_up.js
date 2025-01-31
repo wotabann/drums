893263a82998eb2e9ba71d7739685d2a13dbedc8
wotabann <wotabann@gmail.com>
2025-01-31 20:06:58 +0900

    Add files via upload

--- scroll_up.js
+++ scroll_up.js
@@ -1,20 +1,16 @@
-//scroll_effect
-$(window).scroll(function () {
+//load_effect
+$(document).ready(function(){
   var scrollAnimationElm = document.querySelectorAll('.scroll_up , .scroll_left , .scroll_right');
-  var scrollAnimationFunc = function () {
     for (var i = 0; i < scrollAnimationElm.length; i++) {
       var triggerMargin = 150;
       if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
         scrollAnimationElm[i].classList.add('on');
       }
     }
-  }
-  window.addEventListener('load', scrollAnimationFunc);
-  window.addEventListener('scroll', scrollAnimationFunc);
 });
 
-//load_effect
-$(window).on('load', function (){
+//scroll_effect
+$(window).scroll(function () {
   var scrollAnimationElm = document.querySelectorAll('.scroll_up , .scroll_left , .scroll_right');
   var scrollAnimationFunc = function () {
     for (var i = 0; i < scrollAnimationElm.length; i++) {
